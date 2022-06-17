e9acd3292e4ed790f972ae0d91b72c12d4600f10
eleonore_duvillard <duvillard.eleonore@gmail.com>
2022-06-17 11:50:43 +0200

    ajout des valeurs des produits

--- front/js/products.js
+++ front/js/products.js
@@ -1,3 +1,5 @@
+let products = [];
+
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
         if (res.ok) {
@@ -6,9 +8,45 @@ fetch("http://localhost:3000/api/products")
     })
     .then(function(value) {
         console.log(value);
+        products = value;
+        boucle();
     })
     .catch(function(err) {
         // Une erreur est survenue
     });
 
-    
+function boucle (){
+    for (let i =0; i < products.length; i++) {
+        console.log(i);
+
+    let productLink = document.createElement("a");
+    document.getElementById("items").appendChild(productLink);
+    productLink.href = "http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926";
+    console.log(productLink);
+
+    let productArticle = document.createElement("article");
+    productLink.appendchild(productArticle);
+    console.log(productArticle);
+
+    let productImg = document.createElement("img");
+    productArticle.appendchild(productImg);
+    productImg.src = product.imageUrl;
+    productImg.alt = product.altTxt;
+
+    let productTitle = document.creatElement("h3");
+    productArticle.appendchild(productTitle);
+    productTitle.classList.add("productName");
+    productTitle.innerHTML = product.name;
+    console.log(productTitle);
+
+    let productText = document.creatElement("p");
+    productArticle.appendChild(productText);
+    productText.classList.add("ProductDescription");
+    productText.innerHTML = product.description;
+    console.log(productText);
+    }
+}
+
+let str = window.location.href;
+let url = new URL(str);
+let idProduct = url.searchParams.get("id");
