0c889b1b4e91640a16c9b41e06dee7ac7e8abae5
eleonore_duvillard <duvillard.eleonore@gmail.com>
2022-06-29 09:44:45 +0200

    création ajout d'articles au panier - page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,9 @@
-//Récupération de l'id via les paramètres de l'URL
+// Récupération de l'id via les paramètres de l'URL
 let str = window.location.href;
 let url = new URL(str);
 let idProduct = url.searchParams.get("id");
-//let product;
 
-//Requête de l'API
+// Requête de l'API
 async function productApi() {
   try {
     const response = await fetch(
@@ -17,7 +16,7 @@ async function productApi() {
   }
 }
 
-//Fonction pour la récupération des infos des produits
+// Récupération des infos des produits
 function productInfo(product) {
   let imgContainer = document.querySelector(".item__img");
   let productImg = document.createElement("img");
@@ -41,7 +40,86 @@ function productInfo(product) {
   }
 }
 
-//Appel de la fonction de requête de l'API
+// Appel de la fonction de requête de l'API
 productApi();
 
 
+
+
+/// Ajout d'articles au panier
+
+// Aller chercher l'id du bouton et déclencher l'événement au clic
+let addToCartBtn = document.getElementById("addToCart").addEventListener("click", addToCart);
+
+// Récupération des éléments nécessaires
+let cartId = idProduct;
+console.log(cartId);
+let cartImg = document.querySelector(".item__img");
+console.log(cartImg);
+let cartAlt = document.querySelector(".item__img");
+console.log(cartAlt);
+let cartTitle = document.querySelector("#title");
+console.log(cartTitle);
+let cartColor = document.querySelector("#colors");
+console.log(cartColor);
+let cartPrice = document.querySelector("#price");
+console.log(cartPrice);
+let cartQuantity = document.querySelector("#quantity");
+console.log(cartQuantity);
+
+// Stockage des valeurs des produits dans un objet
+let values = {
+    cartId : idProduct,
+    cartImg : cartImg.src,
+    cartAlt : cartAlt.alt,
+    cartTitle : cartTitle.textContent,
+    cartColor : cartColor.value,
+    cartPrice : cartPrice.textContent,
+    cartQuantity : cartQuantity.value,
+};
+console.log(values);
+
+// Stockage dans un tableau
+const cartArray = [values];
+console.log(cartArray);
+
+// Boucle forEach : j'éxecute la fonction addTocart, pour chaque élement, je recherche si la couleur et la quantité sont déjà ds le panier
+cartArray.forEach((value) =>{
+  addToCart(value.isPresent)
+});
+
+// Création d'un stockage dans le localStorage dans un tableau
+let storage = localStorage.setItem(values, value);
+
+// Recherche de l'élément pour l'ajouter au localStorage
+function addToCart(isPresent){
+  // Est déjà dans le panier : ne modifier que la quantité
+  if ( isPresent = cartId && cartColor.value){
+    this.storage.push(values.cartQuantity)
+  // n'est pas déjà dans le panier alors ajouter tous les éléments)
+  }else{ 
+    this.storage.push(values)
+  }
+}
+
+addToCart();
+
+// Transformation de l'objet au format JSON
+let objectValue = JSON.stringify(values);
+
+
+
+
+
+
+/*
+/// Pour la page panier
+
+// Vérification du contenu du localStorage
+const stockage = window.localStorage;
+console.log(stockage);
+
+// Transformation de l'objet récupéré dans le localStorage au format javascript pour pouvoir l'utiliser
+let objValue = JSON.parse(localStorage.getItem('key'));
+console.log(objValue, "key"); 
+*/
--- front/js/products.js
+++ front/js/products.js
@@ -1,49 +1,51 @@
 // Tableau des produits
 let products;
+console.log(products);
 
 // Appel de l'API pour récupérer les produits
 fetch("http://localhost:3000/api/products")
-    .then(function(res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function(value) {
-        console.log(value);
-        products = value;
-        boucle();
-    })
-    .catch(function(err) {
-        // Une erreur est survenue
-    });
+.then(function(res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value);
+    products = value;
+    boucle();
+})
+.catch(function(err) {
+    // Une erreur est survenue
+});
 
 // Création des cards des produits
 function boucle (){
     for (let i = 0; i < products.length; i++) {
         console.log(i);
-
-    let productLink = document.createElement("a");
-    productLink.href = './product.html?id='+products[i]._id;
-
-    let productArticle = document.createElement("article");
-    
-    let productImg = document.createElement("img");
-    productImg.src = products[i].imageUrl;
-    productImg.alt = products[i].altTxt;
-
-    let productTitle = document.createElement("h3");
-    productTitle.classList.add("productName");
-    productTitle.innerText = products[i].name;
-
-    let productText = document.createElement("p");
-    productText.classList.add("ProductDescription");
-    productText.innerHTML = products[i].description;
-  
-    productArticle.appendChild(productText);
-    productArticle.appendChild(productTitle);
-    productArticle.appendChild(productImg);
-    productLink.appendChild(productArticle);
-    document.getElementById("items").appendChild(productLink);
+        
+        let productLink = document.createElement("a");
+        productLink.href = './product.html?id='+products[i]._id;
+        
+        let productArticle = document.createElement("article");
+        
+        let productImg = document.createElement("img");
+        productImg.src = products[i].imageUrl;
+        productImg.alt = products[i].altTxt;
+        
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add("productName");
+        productTitle.innerText = products[i].name;
+        
+        let productText = document.createElement("p");
+        productText.classList.add("ProductDescription");
+        productText.innerHTML = products[i].description;
+        
+        productArticle.appendChild(productText);
+        productArticle.appendChild(productTitle);
+        productArticle.appendChild(productImg);
+        productLink.appendChild(productArticle);
+        document.getElementById("items").appendChild(productLink);
     }
     console.log(products);
 }
+
