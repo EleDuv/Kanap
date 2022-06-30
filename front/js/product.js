7fd89e43a0efb09ae55589d7aa3ddafdfe505e04
eleonore_duvillard <duvillard.eleonore@gmail.com>
2022-06-30 16:10:53 +0200

    ajout d'articles au panier - page produit

--- front/js/product.js
+++ front/js/product.js
@@ -52,74 +52,48 @@ productApi();
 let addToCartBtn = document.getElementById("addToCart").addEventListener("click", addToCart);
 
 // Récupération des éléments nécessaires
-let cartId = idProduct;
+const cartId = idProduct;
 console.log(cartId);
-let cartImg = document.querySelector(".item__img");
-console.log(cartImg);
-let cartAlt = document.querySelector(".item__img");
-console.log(cartAlt);
-let cartTitle = document.querySelector("#title");
-console.log(cartTitle);
-let cartColor = document.querySelector("#colors");
+const cartColor = document.querySelector("#colors");
 console.log(cartColor);
-let cartPrice = document.querySelector("#price");
-console.log(cartPrice);
-let cartQuantity = document.querySelector("#quantity");
+const cartQuantity = document.querySelector("#quantity");
 console.log(cartQuantity);
 
-// Stockage des valeurs des produits dans un objet
-let values = {
-    cartId : idProduct,
-    cartImg : cartImg.src,
-    cartAlt : cartAlt.alt,
-    cartTitle : cartTitle.textContent,
-    cartColor : cartColor.value,
-    cartPrice : cartPrice.textContent,
-    cartQuantity : cartQuantity.value,
-};
-console.log(values);
+
+// Création d'un tableau de stockage dans le localStorage
+let storage = localStorage.setItem("cartItem", item);
 
 // Stockage dans un tableau
-const cartArray = [values];
+var cartArray = [cartItem];
 console.log(cartArray);
 
-// Boucle forEach : j'éxecute la fonction addTocart, pour chaque élement, je recherche si la couleur et la quantité sont déjà ds le panier
-cartArray.forEach((value) =>{
-  addToCart(value.isPresent)
-});
-
-// Création d'un stockage dans le localStorage dans un tableau
-let storage = localStorage.setItem(values, value);
-
 // Recherche de l'élément pour l'ajouter au localStorage
-function addToCart(isPresent){
-  // Est déjà dans le panier : ne modifier que la quantité
-  if ( isPresent = cartId && cartColor.value){
-    this.storage.push(values.cartQuantity)
-  // n'est pas déjà dans le panier alors ajouter tous les éléments)
-  }else{ 
-    this.storage.push(values)
-  }
+function checkCart(){
+  let getStorage = JSON.parse(localStorage.getItem('cartItem'));
+  cartArray.forEach((item) => {
+    // Est déjà dans le panier : ne modifier que la quantité
+    if (cartId === idProduct && cartItem.cartColor === getStorage.cartColor){
+      this.storage.push(cartItem.cartQuantity++)
+    }
+    // n'est pas déjà dans le panier alors ajouter tous les éléments)
+    else{ 
+      this.storage.push(cartItem)
+    }
+  })
+}
+console.log(checkCart);
+
+function addToCart(){
+  // Stockage des valeurs des produits dans un objet
+  let cartItem = {
+    cartId : idProduct,
+    cartColor : cartColor.value,
+    cartQuantity : cartQuantity.value,
+  };
+  console.log(cartItem);
+  checkCart();
 }
 
-addToCart();
 
 // Transformation de l'objet au format JSON
-let objectValue = JSON.stringify(values);
-
-
-
-
-
-
-/*
-/// Pour la page panier
-
-// Vérification du contenu du localStorage
-const stockage = window.localStorage;
-console.log(stockage);
-
-// Transformation de l'objet récupéré dans le localStorage au format javascript pour pouvoir l'utiliser
-let objValue = JSON.parse(localStorage.getItem('key'));
-console.log(objValue, "key"); 
-*/
+let objectValue = JSON.stringify(cartItem);
